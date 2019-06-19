b580f98628b62df3e8bf4d9cbe354da7237d0743
VinceWen <vince.wen@nokia-sbell.com>
2019-06-19 17:52:30 +0800

    complete 4_1

--- chapter04/4_1.py
+++ chapter04/4_1.py
@@ -22,6 +22,7 @@ class Company(object):
     def __init__(self, employee_list):
         self.employee = employee_list
 
+    # 增加这个方法之后类的实例变成一个可迭代的对象,这个方法充分体现了鸭子类型
     def __getitem__(self, item):
         return self.employee[item]
 
@@ -34,15 +35,15 @@ class Company(object):
 
 a = ["bobby1", "bobby2"]
 b = ["bobby3", "bobby4"]
-name_tuple = ("bobby3", "bobby4")
+name_tuple = ("bobby5", "bobby6")
 
 name_set = set()
-name_set.add("bobby5")
-name_set.add("bobby6")
+name_set.add("bobby7")
+name_set.add("bobby8")
 
 a.extend(b)
-# a.extend(name_tuple)
-# a.extend(name_set)  # list可以extend tuple, set
+a.extend(name_tuple)
+a.extend(name_set)  # list可以extend tuple, set. extend方法接受一个可迭代的对象
 
 company = Company(['a', 'b', 'c'])  # company作为一个可迭代的对象也可以作为一个元素添加到list中
 a.extend(company)
